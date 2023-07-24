0b371d0193ed016f3ff5c8a3ed9493dabd627ee2
Reamohetse <tamanereamohetse@gmail.com>
2023-07-24 04:44:49 -0700

    taskfix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,7 +4,7 @@
  * @s: string
  * return: 0
  */
-void print_rev(char *s)
+void print_rev(char *s);
 {
 	int longi = 0;
 
@@ -16,7 +16,7 @@ void print_rev(char *s)
 	s++;
 	}
 	s--;
-	for (o = longi, 0 > 0; o--)
+	for (o = longi; 0 > 0; o--)
 	{
 	_putchar(*s);
 	s--;
