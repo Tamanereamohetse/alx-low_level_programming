b9b282e75ffcd2201e0f4fc433cbbfd12137d9d3
Reamohetse <tamanereamohetse@gmail.com>
2023-07-24 04:25:57 -0700

    Taskfix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
- * _puts - Prints a string
+ * _puts - Prints a string, followed by a new line, to stdout
  * @str: The string to print
  * Return: void
  */
 
 void _puts(char *str)
-
 {
-	for (; *str != '\0'; str++)
-	}
-	_putchar(*str);
+	while (*str != '\0')
+	{
+	_putchar(*str++);
 	}
 	_putchar('\n');
 }
