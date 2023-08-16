bbbd39b09ddfc192c52175bc20022c4073da991a
Reamohetse <tamanereamohetse@gmail.com>
2023-08-16 05:51:40 -0700

    Task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,13 @@
 /**
  * prints_name - prints name of a person
  * @name: name of a person
+ * @f: pointers to function
  *
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
