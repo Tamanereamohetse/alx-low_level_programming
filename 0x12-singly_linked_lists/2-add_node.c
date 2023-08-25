2258a0767c64cf1d7925c63173b5470857c556f4
Tamanereamohetse <tamanereamohetse@gmail.com>
2023-08-25 12:38:06 -0700

    TASK2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,10 +20,10 @@ list_t *add_node(list_t **head, const char *str)
 	if (!new)
 	return (NULL);
 
-	new ->str = strdup(str);
-	new ->len = len;
-	new ->next = (*head);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
 	(*head) = new;
 
-return (*head)
+return (*head);
 }
