05652f3fd9f7a63344b28a0e5a5e627463d54901
Tamanereamohetse <tamanereamohetse@gmail.com>
2023-09-28 09:39:37 -0700

    Task6

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits
+ * @n: first integer
+ * @m: second integer
+ *
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
